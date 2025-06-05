fd202af7d4323a1942c9c427a33d75b593b9e7c5
Faysal Ahmed <19101043@uap-bd.edu>
2025-06-05 09:55:19 +0600

    First commit from To-Do-List-Project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import "./style.css"
+
+
+export default function App(){
+useState()
+return <>
+    <form className = "new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input type="text" id="item" />
+    </div>
+    <button className="btn">Add</button>
+</form>
+<h1 className="header">Toto List</h1>
+<ul className="list">
+  <li>
+    <label>
+      <input type="checkbox" />
+      Item 1
+    </label>
+    <button className="btn btn-danger">Delete</button>
+  </li>
+
+  <li>
+    <label>
+      <input type="checkbox" />
+      Item 1
+    </label>
+    <button className="btn btn-danger">Delete</button>
+  </li>
+</ul>
+  </>
+}
